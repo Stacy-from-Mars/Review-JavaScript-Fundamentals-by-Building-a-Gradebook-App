432670e696e0ae845c97dc600e519d60f6f2e209
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-06-23 23:19:40 +0300

    step 1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function getAverage(scores) {
-
+    let average = 0;
+    for (let i = 0; i < scores.length; i++) {
+        average += scores[i]
+    }
+    return average / scores.length
 }
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
